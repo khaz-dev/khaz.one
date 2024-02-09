877ddab526353e4215de776344ee387d1cc0cee1
Khairil Azmi Ashari <KhairilAzmiAshari@gmail.com>
2024-02-10 01:02:37 +0700

    add email submit, fix home and certificate margin

--- js/script.js
+++ js/script.js
@@ -89,3 +89,98 @@ const nav = document.querySelector(".nav"),
                     allSection[i].classList.toggle("open");
                 }
             }
+
+/* ============================ Send Email ============================ */
+
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const email = document.getElementById("email");
+const mess = document.getElementById("message");
+
+function sendEmail(){
+const bodyMessage = `Full Name: ${fullName.value}<br> Phone Number: ${phone.value}<br> Email: ${email.value}<br> Message: ${mess.value}`;
+
+    Email.send({
+        SecureToken : "c7b5e3bd-b623-45f9-960a-75bfc0d6a71f",
+        To : 'khairilazmiashari@gmail.com',
+        From : "khairilazmiashari@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK"){
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent succesfully!, I will reply your email as soon as possible :)",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+function checkInputs(){
+    const items = document.querySelectorAll(".form-control");
+
+    for (const item of items){
+        if(item.value == ""){
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[3].value != ""){
+            checkEmail();
+        }
+
+        items[3].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => {
+            if(item.value != ""){
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else{
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail(){
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)){
+        email.classList.add("error")
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blank";
+        }
+    }
+    else{
+        email.classList.remove("error")
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullName.classList.contains("error")  && !phone.classList.contains("error") && !subject.classList.contains("error") && !email.classList.contains("error") && !mess.classList.contains("error")){
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+
+})
